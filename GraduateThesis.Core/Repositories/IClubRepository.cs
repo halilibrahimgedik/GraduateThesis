b67ce02ecbbf6deb187379544a4c9097190265b7
Halil Gedik <halilgedik4234@gmail.com>
2024-05-05 14:07:49 +0300

    Added Entity Repository Classes

---
+++ GraduateThesis.Core/Repositories/IClubRepository.cs
@@ -0,0 +1,14 @@
+﻿using GraduateThesis.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduateThesis.Core.Repositories
+{
+    public interface IClubRepository : IGenericRepository<Club> 
+    {
+
+    }
+}
