533b0b7589a47740769ae9f1a614de1ff0fe3907
Halil Gedik <halilgedik4234@gmail.com>
2024-05-09 11:49:41 +0300

    Added Admin Controller Listing Clubs

---
+++ GraduateThesis.WEB/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using GraduateThesis.WEB.Services.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduateThesis.WEB.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ClubApiService _clubApiService;
+
+        public AdminController(ClubApiService clubApiService)
+        {
+            _clubApiService = clubApiService;
+        }
+
+        public async Task<IActionResult> ManageClubs()
+        {
+            var clubs = await _clubApiService.GetAllAsync();
+            return View(clubs);
+        }
+    }
+}
