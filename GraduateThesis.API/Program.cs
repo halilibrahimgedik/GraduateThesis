bda5804a5e6bb1590f5a364e7168ffbbc0dd9fa7
Halil Gedik <halilgedik4234@gmail.com>
2024-05-17 14:46:08 +0300

    Validating Access Token with custom Extension method

--- GraduateThesis.API/Program.cs
+++ GraduateThesis.API/Program.cs
@@ -2,13 +2,16 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using GraduateThesis.API.Extensions;
 using GraduateThesis.API.Filters;
 using GraduateThesis.API.Middlewares;
 using GraduateThesis.API.Modules;
 using GraduateThesis.Core.Configuration;
 using GraduateThesis.Repository;
 using GraduateThesis.Service.Mapping;
+using GraduateThesis.Service.Services;
 using GraduateThesis.Service.Validators.CategoryDtosValidators;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -32,12 +35,9 @@ builder.Services.AddSwaggerGen();
 
 
 
-// ! options Pattern ile Environment'dan deðer alma
-builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOptions"));
 
 
-
-// Environment deðiþkenlerinden deðerleri al
+// Getting Environment variables
 var customTokenOptions = new CustomTokenOption
 {
     Audiences = Environment.GetEnvironmentVariable("Audiences")?.Split(",").ToList(),
@@ -47,7 +47,7 @@ var customTokenOptions = new CustomTokenOption
     SecurityKey = Environment.GetEnvironmentVariable("SecurityKey")!
 };
 
-
+// ! Options Pattern
 builder.Services.Configure<CustomTokenOption>(options =>
 {
     options.Audiences = customTokenOptions.Audiences;
@@ -59,37 +59,42 @@ builder.Services.Configure<CustomTokenOption>(options =>
 
 
 
+// ! Validating Access Token with custom Extension method
+builder.Services.AddCustomTokenAuthentication(customTokenOptions);
 
 
 
-
-
-
-
-
-// ! AppDbContext yapýlandýrmasý
+// ! AppDbContext Configuration
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     //!LOCAL SERVER
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), option =>
+    if (builder.Environment.IsDevelopment())
     {
-        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
-    });
+        options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), option =>
+        {
+            option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+        });
+    }
 
     //!REMOTE SERVER
-    //options.UseSqlServer(builder.Configuration.GetConnectionString("RemoteSqlServer"), option =>
-    //{
-    //    option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
-    //});
+    if (builder.Environment.IsProduction())
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("RemoteSqlServer"), option =>
+        {
+            option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+        });
+    }
 });
 
 
 // ! FluentValidation
 builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters().AddValidatorsFromAssemblyContaining<CreateCategoryDtoValidator>();
 
+
 // ! NotFoundFilter
 builder.Services.AddScoped(typeof(NotFoundFilter<,>));
 builder.Services.AddScoped(typeof(ValidateCategoryIdsFilter));
+
 
 // ! AutoFac
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
@@ -97,7 +102,6 @@ builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
     containerBuilder.RegisterModule(new ServiceModule());
 });
-
 
 
 
@@ -114,8 +118,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// CustomException Middleware
-app.UseCustomException(); 
+// ! CustomException Middleware
+app.UseCustomException();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
